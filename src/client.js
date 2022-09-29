20e83adbf7245add0e6ca3c2f188ba1e2b0d86b1
Ronald Paek <ronaldpaek@gmail.com>
2022-09-28 18:11:56 -0700

    Changing cdn setting

--- src/client.js
+++ src/client.js
@@ -5,7 +5,7 @@ export const client = sanityClient({
 	projectId: process.env.REACT_APP_SANITY_PROJECT_ID,
 	dataset: 'production',
 	apiVersion: '2022-09-11',
-	useCdn: false,
+	useCdn: true,
 	token: process.env.REACT_APP_SANITY_TOKEN
 });
 
